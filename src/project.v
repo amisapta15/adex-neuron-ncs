3ae765153ae80264f166c34109ed20d07fc6610c
Saptarshi Ghosh <sapta15@gmail.com>
2026-02-25 21:51:25 +0100

    more placeholde updates to pass CI

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_analog_example (
+module tt_um_adex-neuron-ncs (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
